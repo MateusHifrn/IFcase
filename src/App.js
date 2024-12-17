fc8a997409386abb115c60378972766a5f05e632
Mateus Henrique da Silva Fonseca <h.mateus@academico.ifrn.edu.br>
2024-12-17 10:13:37 -0300

    Ifcase

--- src/App.js
+++ src/App.js
@@ -19,15 +19,19 @@ import desenho from './imagens/desenhomacabro.png';
 import pendrive from './imagens/pendrive.png';
 import bottom from './imagens/bottom.png';
 import chaves from './imagens/chaves da moto.png'; 
+import vitima from './imagens/vitima.png'; 
 
 const App = () => {
   const [sceneVisible, setSceneVisible] = useState(false);
   const [hoveredSuspect, setHoveredSuspect] = useState(null);
   const [selectedSuspect, setSelectedSuspect] = useState(null);
   const [gameOver, setGameOver] = useState(false);
+  const [gameOver2, setGameOver2] = useState(false);
   const [message, setMessage] = useState("");
+  const [crimeDetails, setCrimeDetails] = useState("");
   const [visible, setVisible] = useState(false);
   const [activeContent, setActiveContent] = useState(null);
+  const [hoveredVictim, setHoveredVictim] = useState(false);
 
   const handleStartClick = () => {
     setSceneVisible(true);
@@ -45,13 +49,33 @@ const App = () => {
     const culprit = "Cassiano Menta";
     if (suspect === culprit) {
       setMessage(`Parabéns! ${suspect} é o culpado! Você solucionou o caso!`); 
+      setCrimeDetails(`
+        MOTIVAÇÃO DO CRIME - Cassiano e Beatrice
+        
+        Antes da festa, Cassiano e Beatrice haviam descoberto sobre o cofre da mansão e planejaram juntos abri-lo. Porém, durante a festa, Cassiano percebeu que Beatrice queria ficar com tudo sozinha. Isso o deixou furioso. Ele já havia fantasiado a morte dela em seus desenhos macabros, imaginando o pior cenário. Durante uma discussão no andar superior, próximo ao corrimão, a raiva tomou conta e, em um momento de impulso, Cassiano a empurrou, concretizando o que antes era apenas um desenho.
+        
+        Além disso:
+        
+        O pendrive deixado nos destroços indica que ele pode ter deixado cair rapidamente durante a briga junto ao desenho.
+      `);
       setGameOver(true);
     } else {
       setMessage(`Não foi ${suspect}. Continue investigando!`);
-      setGameOver(true);
+      setGameOver2(true);
       simularCrash(); 
     }  
   };
+
+  const pistas = [
+    { id: 1, imagem: maçodecigarro, descricao: "Um maço de cigarro próximo à escadas" },
+    { id: 2, imagem: chaves, descricao: "Uma chave de moto caída perto dos degraus." },
+    { id: 4, imagem: salgadinho, descricao: "Um pacote de salgadinhos amassado, caído no sofá." },
+    { id: 5, imagem: desenho, descricao: "Desenho macabro, caído próximo à escada, com um lado quebrado." },
+    { id: 6, imagem: pendrive, descricao: "Um pendrive com playlists de músicas, encontrado perto dos destroços." },
+    { id: 7, imagem: tecidoverde, descricao: "Um lenço de tecido verde preso no corrimão quebrado." },
+    { id: 8, imagem: peçadexadrez, descricao: "Uma peça de xadrez perto de onde era o corpo." },
+    { id: 9, imagem: bottom, descricao: "Um broche colecionável no chão." },
+  ];
 
   const simularCrash = () => {
     
@@ -66,6 +90,9 @@ const App = () => {
     setActiveContent(activeContent === contentId ? null : contentId);
   };
 
+  const victimImage = vitima; 
+  const victimDescription = 'Idade: 24 anos. Personalidade: Típica popular, carismática e cheia de energia. No entanto, por trás de seu charme e simpatia, havia uma forte dose de arrogância e extremamente manipuladora quando queria.'; 
+
   const suspectImages = {
     "Barbara Lima": BarbaraImage,
     "Carina Leone": CarinaImage,
@@ -74,17 +101,13 @@ const App = () => {
     "Tiago Fritz": TiagoImage,
   };
 
-  const pistas = {
-    "Saco de Batata": salgadinho,
-    "Cofre": cofre,
-  };
-
   const suspectDescriptions = {
     "Barbara Lima": "Idade: 20 anos. Peso: 50 Kg. Personalidade: Calma e amante da natureza. Veio apenas porque seus amigos insistiram.",
     "Carina Leone": "Idade: 22 anos. Peso: 58 Kg. Personalidade: Cérebro do grupo, gosta de enigmas e objetos colecionáveis. Ela foi porque adora explorar lugares antigos.",
     "Cassiano Menta": "Idade: 21 anos. Peso: 67 Kg. Personalidade: Introvertido, amante de música pesada e de artes sombrias. Ele trouxe a música para a festa.",
     "Milo Castelo": "Idade: 23 anos. Peso: 96 Kg. Personalidade: Nerd, apaixonado por tecnologia e jogos de tabuleiro. Ele foi convidado porque trouxe snacks e música.",
     "Tiago Fritz": "Idade: 25 anos. Peso: 72 Kg. Personalidade: Rebelde, aventureiro e desafiador. Ele sugeriu a festa na mansão abandonada.",
+  
   };
 
   return (
@@ -92,7 +115,7 @@ const App = () => {
       <Router>
         <div className="app">
           <header>
-            <h1>DETETIVE VIRTUAL</h1>
+            <h1>IF CASE</h1>
           </header>
 
           <div className="story-container">
@@ -129,7 +152,27 @@ const App = () => {
             </button>
           </nav>
 
+          <div className="victim">
+      <h2>A vítima: </h2>
+      <div className="victim-item">
+        <div
+          className="victim-image"
+          onMouseEnter={() => setHoveredVictim(true)}
+          onMouseLeave={() => setHoveredVictim(false)}
+        >
+          <img src={victimImage} alt="Vítima" />
+          {hoveredVictim && (
+            <div className="victim-description">
+              <p>{victimDescription}</p>
+            </div>
+          )}
+        </div>
+        <p>Beatrice Portinari </p>
+      </div>
+    </div>
+
           <div className="suspects">
+
             <h2>Suspeitos</h2>
             <div className="suspect-list">
               {Object.keys(suspectImages).map((suspect) => (
@@ -213,18 +256,21 @@ const App = () => {
           )}
 
           {sceneVisible && (
-            <div className="pistasBotoes">
-              <h3>Escolha o culpado:</h3>
-              {Object.keys(pistas).map((suspect) => (
-                <button
-                  key={suspect}
-                  className="suspect-button"
-                  onClick={() => handleSuspectSelection(suspect)}
-                >
-                  {suspect}
-                </button>
-              ))}
-            </div>
+           
+              <div className="pistas-container">
+      {pistas.map((pista) => (
+        <div className="pista-item" key={pista.id}>
+          <img
+            src={pista.imagem}
+            alt={`Pista ${pista.id}`}
+            className="pista-imagem"
+          />
+          <div><p className="pista-descricao">{pista.descricao}</p></div>
+          
+        </div>
+      ))}
+    </div>
+            
           )}
 
           {sceneVisible && (
@@ -245,11 +291,20 @@ const App = () => {
           {gameOver && (
             <div className="game-over-message">
               <p>{message}</p>
+              <p className='crimedetailss'>{crimeDetails}</p>
               {typeof message === 'string' && message.includes("Continue investigando") && (
                 <button className="restart-button" onClick={handleRestartClick}>
                   Tente Novamente!
                 </button>
               )}
+            </div>
+          )}
+
+         {gameOver2 && (
+            <div className="game-over-message">
+              {message && <p style={{ color: "red", fontWeight: "bold" }}>{message}</p>}
+            
+             
             </div>
           )}
 
